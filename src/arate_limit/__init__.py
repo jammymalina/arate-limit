4227738cc58c3cd66628534084afe062d42fad35
jammymalina <jakub.jantosik@gmail.com>
2025-01-16 22:47:10 +0100

    Update

--- src/arate_limit/__init__.py
+++ src/arate_limit/__init__.py
@@ -1,2 +1,99 @@
-def hello() -> str:
-    return "Hello from arate-limit!"
+import asyncio
+import time
+from datetime import timedelta
+
+
+def seconds_to_nanoseconds(seconds: float) -> int:
+    return int(seconds * 1e9)
+
+
+def nanoseconds_to_seconds(nanoseconds: int) -> float:
+    return nanoseconds / 1e9
+
+
+class AtomicInt:
+    _value: int
+    _lock: asyncio.Lock
+
+    def __init__(self, value: int = 0) -> None:
+        self._value = value
+        self._lock = asyncio.Lock()
+
+    async def inc(self, delta: int = 1) -> int:
+        async with self._lock:
+            self._value += delta
+            return self._value
+
+    async def dec(self, delta: int = 1) -> int:
+        return await self.inc(-delta)
+
+    async def get_value(self) -> int:
+        async with self._lock:
+            return self._value
+
+    async def set_value(self, value: int) -> int:
+        async with self._lock:
+            self._value = value
+
+    async def compare_and_swap(self, old: int, new: int) -> bool:
+        async with self._lock:
+            if self._value == old:
+                self._value = new
+                return True
+
+            return False
+
+
+class AtomicIntRateLimiter:
+    _per_request: int
+    _max_slack: int
+    _state: AtomicInt
+
+    def __init__(self, rate_limit: int, time_window: int | float | timedelta = 1.0, slack: int = 10) -> None:
+        """
+        Initialize a rate limiter with specified parameters.
+
+        Args:
+            rate_limit (int): Maximum number of requests allowed in the time window
+            time_window (int | float | timedelta): Time period in seconds for the rate limit (default: 1.0)
+            slack (int): Additional allowance for brief bursts (default: 10)
+        """
+
+        if rate_limit <= 0:
+            raise ValueError("Rate limit must be positive")
+        if slack < 0:
+            raise ValueError("Slack must be non-negative")
+
+        tw = time_window
+        if isinstance(time_window, (int, float)):
+            tw = timedelta(seconds=time_window)
+
+        if tw.total_seconds() <= 0:
+            raise ValueError("Time window must be positive")
+
+        self._per_request = seconds_to_nanoseconds(tw.total_seconds()) // rate_limit
+        self._max_slack = slack * self._per_request
+        self._state = AtomicInt()
+
+    async def take(self) -> None:
+        new_time_of_next_permission_issue = 0
+
+        while True:
+            now = time.monotonic_ns()
+            time_of_next_permission_issue = await self._state.get_value()
+
+            if time_of_next_permission_issue == 0 or (
+                self._max_slack == 0 and now - time_of_next_permission_issue > self._per_request
+            ):
+                new_time_of_next_permission_issue = now
+            elif self._max_slack > 0 and now - time_of_next_permission_issue > self._max_slack + self._per_request:
+                new_time_of_next_permission_issue = now - self._max_slack
+            else:
+                new_time_of_next_permission_issue = time_of_next_permission_issue + self._per_request
+
+            if await self._state.compare_and_swap(time_of_next_permission_issue, new_time_of_next_permission_issue):
+                break
+
+        sleep_duration = new_time_of_next_permission_issue - now
+        if sleep_duration > 0:
+            await asyncio.sleep(nanoseconds_to_seconds(sleep_duration))
--- tests/test_arate_limit.py
+++ tests/test_arate_limit.py
@@ -1,5 +1,52 @@
-from arate_limit import hello
+import asyncio
+from datetime import datetime
+
+import pytest
+from pytest_mock import MockerFixture
+
+from arate_limit import AtomicInt, AtomicIntRateLimiter
 
 
-def test_true() -> None:
-    assert hello() == "Hello from arate-limit!"
+async def test_atomic_int_init() -> None:
+    value_default = AtomicInt()
+    assert await value_default.get_value() == 0
+
+    value = AtomicInt(10)
+    assert await value.get_value() == 10
+
+
+async def test_atomic_int() -> None:
+    value = AtomicInt()
+    assert await value.inc() == 1
+    assert await value.dec() == 0
+    assert await value.inc(10) == 10
+    assert await value.dec(5) == 5
+    assert await value.get_value() == 5
+    await value.set_value(12)
+    assert await value.get_value() == 12
+    assert await value.compare_and_swap(12, 10)
+    assert not await value.compare_and_swap(12, 10)
+
+
+async def test_atomic_int_rate_limiter_init() -> None:
+    rate_limiter = AtomicIntRateLimiter(15, time_window=2.0, slack=10)
+
+    assert rate_limiter._max_slack == 1333333330
+    assert rate_limiter._per_request == 133333333
+    assert await rate_limiter._state.get_value() == 0
+
+
+async def test_atomic_int_rate_limiter(mocker: MockerFixture) -> int:
+    call_counter = mocker.AsyncMock()
+    rate_limiter = AtomicIntRateLimiter(20)
+
+    async def _call() -> None:
+        await rate_limiter.take()
+        await call_counter()
+
+    start = datetime.now()
+    await asyncio.gather(*(_call() for _ in range(100)))
+    end = datetime.now()
+
+    assert (end - start).total_seconds() == pytest.approx(5.0, 0.2)
+    assert call_counter.await_count == 100
